72def930127b9cc26200b0d7175f31feddbe480f
RDCbum <duran.robert301@gmail.com>
2025-12-25 17:39:23 +0100

    paper: integrate Lean statement, assumptions ledger, and reproducibility

---
+++ docs/core/PrintMasterStatement.lean
@@ -0,0 +1,5 @@
+import ERURH.ERURH_MasterTheoremSummary
+#print ERURH.RH_from_ERURH_conditional
+#print ERURH.ERURH_GlobalAssumptions
+#print ERURH.ERURH_Assumptions
+#print ERURH.WindowScalingAssumptions

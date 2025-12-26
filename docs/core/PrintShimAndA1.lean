2ec58a85425a8e94a620ac7e30a76d48d80e276d
RDCbum <duran.robert301@gmail.com>
2025-12-27 00:12:50 +0100

    docs: RB18 export exact shim + A1 statements

---
+++ docs/core/PrintShimAndA1.lean
@@ -0,0 +1,8 @@
+import ERURH.AxiomsShimBundle
+import ERURH.A1FromSupercriticalMode
+import ERURH.Alpha.GeneratedRMSContext
+
+#print ERURH.AxiomsShimAccepted
+#print ERURH.A1_mode_of_supercritical
+#print ERURH.A1_mode
+#print ERURH.Alpha.GeneratedRMSContext.ctx_real
